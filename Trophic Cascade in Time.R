184a36b698f6cca75563cb6abab603efaf16926c
cpsh1234 <58405996+cpsh1234@users.noreply.github.com>
2020-05-01 11:02:11 +0100

    Update and rename Trophic Cascade in Time to Trophic Cascade in Time.R

--- Trophic Cascade in Time.R
+++ Trophic Cascade in Time.R
@@ -1,11 +1,9 @@
 library(mizer)
-params_knife <- set_community_model(z0 = 0.1, recruitment = 4e7, alpha = 0.2, f0 = 0.7, knife_edge_size = 1000, sigma = 2, h=10, beta = 100, zeta = 2, n=2/3)
+params_knife <- set_community_model(z0 = 0.1, recruitment = 4e7, alpha = 0.2, f0 = 0.7, knife_edge_size = 1000, sigma = 2, h=10, beta = 100, zeta = 2, n=0.88)
 
+simNoFishing <- project(params_knife, effort = 0, t_max = 300, dt=0.01)
+simFishing<- project(params_knife, effort = 1, t_max = 300, dt=0.01)
 
-simNoFishing <- project(params_knife, effort = 0, t_max = 100, dt=0.01)
-simFishing<- project(params_knife, effort = 1, t_max = 100, dt=0.01)
-
-plot(simFishing)
 
 plankton_no_fishing <- simNoFishing@n_pp
 plankton_fishing <- simFishing@n_pp
@@ -42,7 +40,7 @@ for (i in 1:length(fish_no_fishing[1,])) {
         if(fish_no_fishing[j,i] == 0){
             fish_no_fishing[j,i] <- NaN
         }
-        fish_no_fishing[j,i] <- fish_no_fishing[j,i]*simNoFishing@params@w[i]^(2.8-2/3)
+        fish_no_fishing[j,i] <- fish_no_fishing[j,i]*simNoFishing@params@w[i]
     }
 }
 
@@ -52,7 +50,7 @@ for (i in 1:length(fish_fishing[1,])) {
         if(fish_fishing[j,i] == 0){
             fish_fishing[j,i] <- NaN
         }
-        fish_fishing[j,i] <- fish_fishing[j,i]*simNoFishing@params@w[i]^(2.8-2/3)
+        fish_fishing[j,i] <- fish_fishing[j,i]*simNoFishing@params@w[i]
     }
 }
 
@@ -62,13 +60,13 @@ relative_abundance <- ((fish_fishing-fish_no_fishing) / (fish_no_fishing+fish_fi
 
 plankton_relative_abundance <- (plankton_fishing-plankton_no_fishing) / (plankton_no_fishing+plankton_fishing)
 
-p <- plot_ly(x = log10(simNoFishing@params@w), y = 0:(length(relative_abundance[,1])-1),z = log10(relative_abundance),colorscale='Portland') %>% 
+p <- plot_ly(x = log10(simNoFishing@params@w), y = 0:(length(relative_abundance[,1])-1),z = (relative_abundance),colorscale='Portland') %>% 
     layout(
     title = "Relative Abundance",
     scene = list(
         xaxis = list(title = "log(weight) [log(g)]"),
         yaxis = list(title = "time [years]"),
-        zaxis = list(title = "log(biomass density) [log(g/m^3)]")
-    )) %>% add_surface() %>%
-    add_surface(x = log10(simNoFishing@params@w_full), y = 0:(length(plankton_relative_abundance[,1])-1),z = log10(plankton_relative_abundance),colorscale='Greens')
+        zaxis = list(title = "relative abundance")
+    )) %>% add_surface() %>% 
+    add_surface(x = log10(simNoFishing@params@w_full), y = 0:(length(plankton_relative_abundance[,1])-1),z = (plankton_relative_abundance),colorscale='Greens')
 p
