6dff74a3a34537f5dca793fd72d94883c242a8f9
cpsh1234 <58405996+cpsh1234@users.noreply.github.com>
2020-05-01 11:03:17 +0100

    Update Size-mograph.R

--- Size-mograph.R
+++ Size-mograph.R
@@ -1,8 +1,8 @@
 library(mizer)
-params_knife <- set_community_model(z0 = 0.1, recruitment = 4e7, alpha = 0.2, f0 = 0.7, knife_edge_size = 1000, sigma = 3, h=10, beta = 100, zeta = 4000, n=0.88)
+params_knife <- set_community_model(z0 = 0.1, recruitment = 4e7, alpha = 0.2, f0 = 0.7, knife_edge_size = 1000, sigma = 2, h=10, beta = 100, zeta = 2, n=0.72)
 
 
-simNoFishing <- project(params_knife, effort = 0, t_max = 200, dt=0.01)
+simNoFishing <- project(params_knife, effort = 1, t_max = 300, dt=0.01)
 
 
 plankton_no_fishing <- simNoFishing@n_pp
@@ -34,10 +34,10 @@ for (i in 1:length(fish_no_fishing[1,])) {
 
 library(plotly)
 
-#Plots the Biomass spectrum over time
+
 pb <- plot_ly(x = log10(simNoFishing@params@w), y = 0:(length(fish_no_fishing[,1])-1),z = log10(fish_no_fishing),colorscale='Blues') %>% 
     layout(
-        title = "No Fishing Abundance",
+        title = "Biomass Density Against Time",
         scene = list(
             xaxis = list(title = "log(weight) [log(g)]"),
             yaxis = list(title = "time [years]"),
@@ -55,22 +55,37 @@ for(i in 1:length(plankton_no_fishing[1,])){
     }
 }
 
-#Discard the first 49 time steps as this is used by the system to find the fixed point of cycle.
+
 time = c(49:(length(plankton_no_fishing[,1])-1))
-y1 = ((log(plankton_no_fishing[,planton_index]) - log(plankton_no_fishing[50,planton_index])))[-1:-49]
+y1 = ((log(plankton_no_fishing[,planton_index]) - log(plankton_no_fishing[77,planton_index])))[-1:-49]
 
 y2 = ((log(fish_no_fishing[,3]) - log(fish_no_fishing[50,3])))[-1:-49]
 y3 = ((log(fish_no_fishing[,50]) - log(fish_no_fishing[50,50])))[-1:-49]
 y4 = ((log(fish_no_fishing[,90]) - log(fish_no_fishing[50,90])))[-1:-49]
 
-data <- data.frame(time, Init_Plankton, y1, y2, y3, y4)
 
+f <- list(
+    family = "Courier New, monospace",
+    size = 18,
+    color = "#7f7f7f"
+)
+x <- list(
+    title = "time",
+    titlefont = f
+)
+y <- list(
+    title = "log(biomass density change)",
+    titlefont = f
+)
 
-#Plots the "size-mograph"
+data <- data.frame(time, y1, y2, y3, y4)
+
 plot2 <- plot_ly(data, x = ~time)  %>%
     add_trace(y = ~y1, name = 'planton w = 0.66mg', type = 'scatter',mode = 'lines') %>%
     add_trace(y = ~y2, name = 'fish w = 1.52mg', type = 'scatter',mode = 'lines') %>%
     add_trace(y = ~y3, name = 'fish w = 28.5g', type = 'scatter',mode = 'lines') %>%
     add_trace(y = ~y4, name = 'fish w = 123kg', type = 'scatter',mode = 'lines')
+
+plot2 <- plot2 %>% layout(xaxis = x, yaxis = y)
 
 plot2
